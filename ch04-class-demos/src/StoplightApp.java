14d63b174b0408ce28ae262e852f46820c0c0aa2
LindaCr <lindacrook.44@gmail.com>
2021-06-06 22:44:49 -0400

    added more code

--- ch04-class-demos/src/StoplightApp.java
+++ ch04-class-demos/src/StoplightApp.java
@@ -7,12 +7,39 @@ public class StoplightApp {
 		System.out.println();
 		Scanner sc=new Scanner(System.in);
 		
+		
 		String choice="y";
 		
 		while (choice.equalsIgnoreCase("y")) {
 			
-			System.out.println("What color is the stoplight? ");
+			System.out.println("What color is the stoplight? red/yellow/green");
 			String color=sc.next();
+			
+			
+			switch (color) {
+			case "red":
+			case "Red":
+				System.out.println("Are you turning right? true/false");
+				String right=sc.next();
+				
+				System.out.println("Is there anyone coming?");
+				String obstacle=sc.next();
+				
+				
+				break;
+				
+			case "yellow":
+			case "Yellow":
+				System.out.println("Are you more than 35 yards away? true/false");
+				String yards=sc.next();
+				
+				break;
+			case "green":
+			case "Green":
+				System.out.println("Are there any obstacles? true/false");
+				String gogo=sc.next();
+				break;
+			}
 			
 			
 			
