a125e23d2dca2362f0f50d277807a43b31f8496e
LindaCr <lindacrook.44@gmail.com>
2021-06-07 15:02:07 -0400

    added code

--- ch04-Factorial-Calculator/src/FactorialCalculatorApp.java
+++ ch04-Factorial-Calculator/src/FactorialCalculatorApp.java
@@ -7,20 +7,18 @@ public class FactorialCalculatorApp {
 		System.out.println();
 		Scanner sc= new Scanner(System.in);
 		
-		System.out.println("Enter an integer that's greater than 0 and less than 10:");
-		int number=sc.nextInt();
 		
 		String choice="y";
 		while (choice.equalsIgnoreCase("y")) {
 			
+			System.out.println("Enter an integer that's greater than 0 and less than 10:");
+			int number=sc.nextInt();
 			
-			
-			
-			
-			
-			
-			
-			
+			long factorial= 1;
+			for (int i=1; i<=number; i++) {
+				factorial *=i;
+			}
+			System.out.println("The factorial of "+number+" is "+factorial);
 			
 			
 			System.out.println("Continue? y/n");
