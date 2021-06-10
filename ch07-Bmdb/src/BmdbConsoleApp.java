cd0374c3f53a8133d42ed71821c5c71e431e9f81
LindaCr <lindacrook.44@gmail.com>
2021-06-09 22:26:18 -0400

    changed code

--- ch07-Bmdb/src/BmdbConsoleApp.java
+++ ch07-Bmdb/src/BmdbConsoleApp.java
@@ -7,27 +7,23 @@ public class BmdbConsoleApp {
 		System.out.println();
 		Scanner sc= new Scanner(System.in);
 		
-		int iD=Console.getInt("ID? ");
-		String title=Console.getString("Title? ");
+		int id=Console.getInt("ID? ");
+		String title=Console.getLine("Title? ");
 		int year=Console.getInt("Year? ");
 		String rating=Console.getString("Rating? ");
-		String director=Console.getString("Director? ");
+		String director=Console.getLine("Director? ");
 		
-		Movie m1=new Movie(iD, title, year, rating, director);
+		Movie m1=new Movie(id, title, year, rating, director);
 		
-		printMovie(m1);
+		System.out.println("Movie Summary: ");
+		System.out.println("===============");
+		System.out.println(m1);
 		
-		
+	System.out.println();	
 	System.out.println("Peace");
 	}
 	
-	private static void printMovie(Movie m) {
-		System.out.println("ID: "+m.getID());
-		System.out.println("Title: "+m.getTitle() );
-		System.out.println("Year: "+m.getYear() );
-		System.out.println("Rating: "+m.getRating() );
-		System.out.println("Director: "+m.getDirector());
-	}
+
 	
 	
 }
