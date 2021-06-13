7dc8f023c83c6f324e5d2f3c53c1d7763b92e8c8
LindaCr <lindacrook.44@gmail.com>
2021-06-12 22:22:30 -0400

    fixed a line of code

--- ch09_ex1_DisplayableTest/src/DisplayableTestApp.java
+++ ch09_ex1_DisplayableTest/src/DisplayableTestApp.java
@@ -6,8 +6,7 @@ public class DisplayableTestApp {
         Employee e = new Employee(2, "Smith", "John");
         // TODO: add code that passes this object to the display method below
 
-        Product p = new Product("java", "Murach's Java Programming", 57.50);
-        // TODO: add code that passes this object to the display method below
+       
         
         System.out.println();        
     }
