4f767ec78ce6d94b89df7b95fc1328474b7e98b2
LindaCr <lindacrook.44@gmail.com>
2021-06-15 22:57:10 -0400

    init commit

---
+++ ch12-wizard-inventory/src/ui/WizardInventoryApp.java
@@ -0,0 +1,18 @@
+package ui;
+
+public class WizardInventoryApp {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Wizard Inventory Game");
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Peace");
+
+	}
+
+}
