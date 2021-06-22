6e40bacb2f113e6c112a787e4ff1b3639f27378e
LindaCr <lindacrook.44@gmail.com>
2021-06-21 22:21:13 -0400

    init commit

---
+++ bmdb-console/src/db/ActorDB.java
@@ -0,0 +1,40 @@
+package db;
+
+import java.util.List;
+
+import business.Actor;
+import interfaces.DAO;
+
+public class ActorDB implements DAO<Actor>{
+
+	@Override
+	public Actor get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean add(Actor actor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(Actor actor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(Actor actor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
