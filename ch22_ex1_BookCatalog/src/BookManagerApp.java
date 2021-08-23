c1010f5a0df93877014ad173ded4dc2fb0da5555
LindaCr <lindacrook.44@gmail.com>
2021-08-22 21:39:03 -0400

    cleaned up code

--- ch22_ex1_BookCatalog/src/BookManagerApp.java
+++ ch22_ex1_BookCatalog/src/BookManagerApp.java
@@ -10,16 +10,19 @@ public class BookManagerApp {
                 b -> b.getTitle().contains("Java"));
         System.out.println("\nBOOKS BY TITLE:");
         printList(booksByTitle);
+        System.out.println();
         
         List<Book> booksByCategory = manager.getBooks(
                 b -> b.getCategory().equals(Book.JAVA));
         System.out.println("\nBOOKS BY CATEGORY:");
         printList(booksByCategory);
+        System.out.println();
         
         List<Book> booksByFormat = manager.getBooks(
                 b -> b.getFormat().equals(Book.PAPERBACK));
         System.out.println("\nBOOKS BY FORMAT:");
         printList(booksByFormat);
+        System.out.println();
     }
     
     public static void printList(List<Book> bookList) {
